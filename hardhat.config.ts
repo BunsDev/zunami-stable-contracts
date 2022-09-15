4b239a4865a59202a96bd7e3ea00292d494ec3dd
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2022-09-15 13:36:08 +0300

    Deploy test Elastic token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -129,8 +129,7 @@ const config: HardhatUserConfig = {
         timeout: 500000,
     },
     etherscan: {
-        apiKey: `${process.env.BSCSCAN_API_KEY}`,
-        // apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+        apiKey: `${process.env.ETHERSCAN_API_KEY}`,
     },
 };
 
---
+++ scripts/deployUZD.js
@@ -0,0 +1,16 @@
+async function main() {
+    console.log('Start deploy');
+    const name = 'UZD';
+    const Token = await ethers.getContractFactory(name);
+    const token = await Token.deploy();
+
+    await token.deployed();
+    console.log(`Token ${name} deployed to:`, token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
