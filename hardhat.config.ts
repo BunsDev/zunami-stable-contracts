7cec805c30c5a39f89eb20a6aba90bae085ce375
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2023-03-07 11:10:39 +0200

    Deploy UZD v1.2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -83,7 +83,7 @@ const config: HardhatUserConfig = {
             chainId: 1,
             gas: 'auto',
             gasMultiplier: 1.2,
-            gasPrice: 15000000000,
+            gasPrice: 30000000000,
             accounts: [`${process.env.PRIVATE_KEY}`],
             loggingEnabled: true,
         },
